9de8735bebb072ab8788531039dd952bbce8c85f
mdodell <dodellmitchell@gmail.com>
2021-03-11 22:10:44 -0700

    Updated blitz config

--- blitz.config.js
+++ blitz.config.js
@@ -7,10 +7,4 @@ module.exports = withTM({
       isAuthorized: simpleRolesIsAuthorized,
     }),
   ],
-  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-  //   // Note: we provide webpack above so you should not `require` it
-  //   // Perform customizations to webpack config
-  //   // Important: return the modified config
-  //   return config
-  // },
 })
